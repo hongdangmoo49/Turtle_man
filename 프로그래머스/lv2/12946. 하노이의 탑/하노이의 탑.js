cede0243dcd6f632d92c27997dcba0213be57e1b
hongdangmoo49 <118052344+hongdangmoo49@users.noreply.github.com>
2023-09-17 19:18:10 +0900

    [level 2] Title: 하노이의 탑, Time: 5.02 ms, Memory: 48.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.js
@@ -0,0 +1,20 @@
+function solution(n) {
+    const answer = [];
+
+    function hanoi(n, start, mid, end) {
+
+        if (n === 1) {
+            answer.push([start, end]);
+            return;
+        }
+
+        hanoi(n-1, start, end, mid);
+
+        answer.push([start, end]);
+        
+        hanoi(n-1, mid, start, end);
+    }
+
+    hanoi(n, 1, 2, 3);
+    return answer;
+}

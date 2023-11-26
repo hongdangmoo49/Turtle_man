33a2193d8af3e65b53062638c6e23c9ead929057
hongdangmoo49 <118052344+hongdangmoo49@users.noreply.github.com>
2023-11-26 20:58:44 +0900

    [level 2] Title: 프로세스, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -0,0 +1,21 @@
+function solution(priorities, location) {
+
+    let queue = priorities.map((priority, index) => ({priority, index}));
+    let order = 0;
+
+    while (queue.length > 0) {
+
+        let current = queue.shift();
+
+        if (queue.some(process => process.priority > current.priority)) {
+            queue.push(current);
+        } else {
+
+            order++;
+
+            if (current.index === location) {
+                return order;
+            }
+        }
+    }
+}
